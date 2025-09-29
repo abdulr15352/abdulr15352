0b98b593efc8782b501d250e11370e859610eb84
Abdul-Rafeh Alvi <128264832+abdulr15352@users.noreply.github.com>
2025-09-29 17:38:39 -0500

    rydde koden

--- Python/mac_changer/mac_changer.py
+++ Python/mac_changer/mac_changer.py
@@ -10,11 +10,11 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
+def change_mac(interface, new_mac):
+    print(f"[+]Changing interface for {interface} to {new_mac}")
 
-print(f"[+]Changing interface for {interface} to {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-subprocess.call(["ifconfig", interface, "up"])
+change_mac(options.interface, options.new_mac)
