ef777f2991c8a51a12184994cfc4606826e14977
Abdul-Rafeh Alvi <128264832+abdulr15352@users.noreply.github.com>
2025-09-29 17:28:20 -0500

    added cli arguments

--- Python/mac_changer/mac_changer.py
+++ Python/mac_changer/mac_changer.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 import subprocess
+import optparse
 
-interface = input("Interface Name: ")
-new_mac = input("New mac address: ")
+parser = optparse.OptionParser()
+
+parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")
+parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+
+(options, arguments) = parser.parse_args()
+
+interface = options.interface
+new_mac = options.new_mac
 
 print(f"[+]Changing interface for {interface} to {new_mac}")
 
