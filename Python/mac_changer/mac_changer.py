159ae95fa508abdfbce9ff6a71b95c9e6d3f939b
Abdul-Rafeh Alvi <128264832+abdulr15352@users.noreply.github.com>
2025-09-28 19:30:01 -0500

    implemented input validation

--- Python/mac_changer/mac_changer.py
+++ Python/mac_changer/mac_changer.py
@@ -7,6 +7,6 @@ new_mac = input("New mac address: ")
 
 print(f"[+]Changing interface for {interface} to {new_mac}")
 
-subprocess.call("ifconfig " + interface + " down", shell=True)
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig", interface, "down"])
+subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+subprocess.call(["ifconfig", interface, "up"])
