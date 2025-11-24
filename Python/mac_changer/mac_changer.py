9e40b03238d326fab801f12c6734a470e787c82e
Abdul-Rafeh Alvi <128264832+abdulr15352@users.noreply.github.com>
2025-11-24 03:34:30 +0100

    Enhance MAC changer with current MAC retrieval
    Added functionality to get the current MAC address and verify the change.

--- Python/mac_changer/mac_changer.py
+++ Python/mac_changer/mac_changer.py
@@ -1,24 +1,43 @@
-#!/usr/bin/env python  
+#!/usr/bin/env python
 
-import subprocess  # subprocess module for running system commands
-import optparse  # optparse for parsing command line options
+import subprocess
+import optparse
+import re
 
-def get_arguments():  
-    parser = optparse.OptionParser()  
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")  # adds option for interface
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")  # adds option for new mac
-    (options, arguments) = parser.parse_args()  
-    if not options.interface:  # checks if values for interface is provided
-        parser.error("[-] Please specify an interface, use --help for more information.") 
-    if not options.new_mac:  # checks if value for mac address is provided
-        parser.error("[-] Please specify a new MAC address, use --help for more information.")  
-    return options  
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its mac address")
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify an interface, use --help for more information.")
+    if not options.new_mac:
+        parser.error("[-] Please specify a new MAC address, use --help for more information.")
+    return options
 
-def change_mac(interface, new_mac): 
-    print(f"[+]Changing interface for {interface} to {new_mac}")  
-    subprocess.call(["ifconfig", interface, "down"])  # brings the interface down
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])  # sets the new mac
-    subprocess.call(["ifconfig", interface, "up"])  # brings the interface up again after implementing changes
+def change_mac(interface, new_mac):
+    print(f"[+] Changing interface for {interface} to {new_mac}")
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+    subprocess.call(["ifconfig", interface, "up"])
 
-options = get_arguments()  
-change_mac(options.interface, options.new_mac)  
+def get_current_mac(interface):
+    ifconfig_result = subprocess.check_output(["ifconfig", interface]).decode("utf-8")
+    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", ifconfig_result)
+    if mac_address_search_result:
+        return mac_address_search_result.group(0)
+    else:
+        print("[-] No mac address found")
+
+
+options = get_arguments()
+current_mac = get_current_mac(options.interface)
+print(f"[+] Current MAC address: {current_mac}")
+
+change_mac(options.interface, options.new_mac)
+current_mac = get_current_mac(options.interface)
+if current_mac == options.new_mac:
+    print(f"[+] MAC address changed successfully to {current_mac}")
+else:
+    print(f"[-] MAC address change failed, please try again")
+
